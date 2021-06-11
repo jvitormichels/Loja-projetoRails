bf62b63a70009d29dbb70bd8a6b21b25a81b32d8
jvitormichels <jvitormichels@gmail.com>
2021-06-11 10:58:00 -0300

    Upload files

---
+++ app/controllers/produtos_controller.rb
@@ -0,0 +1,25 @@
+class ProdutosController < ApplicationController
+
+    def index
+        @produtos = Produto.order(nome: :asc)
+        @produto_desconto = Produto.order(:preco).limit 1
+    end
+
+    def create
+        produto = params.require(:produto).permit(:nome, :descricao, :preco, :quantidade)
+        Produto.create produto
+        redirect_to root_path
+    end
+
+    def destroy
+        id = params[:id]
+        Produto.destroy id
+        redirect_to root_path
+    end
+
+    def busca
+        @nome = params[:nome]
+        @produtos = Produto.where "nome like ?", "%#{@nome}%"
+    end
+
+end
