0a1b6c9649006cdf3ec269fe25c5dc782307f5bd
jvitormichels <jvitormichels@gmail.com>
2021-06-12 16:58:48 -0300

    feat: list products per department

--- app/controllers/departamentos_controller.rb
+++ app/controllers/departamentos_controller.rb
@@ -8,6 +8,7 @@ class DepartamentosController < ApplicationController
 
   # GET /departamentos/1 or /departamentos/1.json
   def show
+    @produtos_por_departamento = Produto.where("departamento_id = ?", params[:id])
   end
 
   # GET /departamentos/new
